78f838e0eacbd1c93ff7530df3bdeeef4cb91665
DELL <220128871@mycput.ac.za>
2023-03-29 14:21:50 +0200

    first commit

---
+++ Downloads/CareGivers/CareGivers/src/main/java/com/mycompany/caregivers/Main.java
@@ -0,0 +1,15 @@
+/* Main.java
+ * This is the main section
+
+ * 8 August 2022
+*/
+package com.mycompany.caregivers;
+
+public class Main {
+
+       public static void main(String[] args) {
+
+           new CareGiverGUI().setGUI();
+    }
+    
+}
